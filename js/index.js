6a9f0c4460ca80ba05255b53aa4f1798db0db28f
Alfred <ya95123@gmail.com>
2021-07-22 14:40:46 +0800

    menu InOut

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-const suns = document.querySelectorAll(".sun")
 const runnerFrame = document.querySelector(".runnerFrame")
 const sunSpotLine = document.querySelector(".sunSpotLine")
 const btnMenu = document.querySelector(".btn-menu")
+
+const suns = document.querySelectorAll(".sun")
+const items = document.querySelectorAll(".item")
 
 // parallex
 const parallaxSunIndex = new Parallax(suns[0])
@@ -12,8 +14,21 @@ const isSpotLine = () => {
   sunSpotLine.classList.toggle("show9")
 }
 
+const menuIn = () => {
+  let second = 0
+  items.forEach(item => {
+    if (item.getAttribute("class").includes("none")) return
+
+    setTimeout(() => {
+      item.classList.toggle("item-in")
+    }, second)
+    second += 100
+  })
+}
+
 // hover runner + spotLine
 runnerFrame.addEventListener("mouseover", isSpotLine, false)
 runnerFrame.addEventListener("mouseout", isSpotLine, false)
 btnMenu.addEventListener("mouseover", isSpotLine, false)
 btnMenu.addEventListener("mouseout", isSpotLine, false)
+btnMenu.addEventListener("click", menuIn, false)
